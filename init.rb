809ec57ba569583534b33e46347d1225005775a2
ameya86 <a_house_of_tomato@lion.ocn.ne.jp>
2012-06-19 19:19:01 +0900

    redmine2.0.x対応版ですが、自己責任版でもあります。

--- init.rb
+++ init.rb
@@ -1,13 +1,12 @@
-require 'redmine'
 require 'parallel_issue_hook'
 require 'parallel_issue_issue_patch'
 require 'parallel_issues_controller_patch'
 
-Redmine::Plugin.register :redmine_parallel_issue do
-  name 'Redmine Parallel Issue plugin'
+Redmine::Plugin.register :redmine_parallel_issue2 do
+  name 'Redmine Parallel Issue2 plugin'
   author 'OZAWA Yasuhiro'
   description 'Multiple create issue for many subject and assignees,'
-  version '0.0.2'
+  version '0.0.1'
   url 'https://github.com/ameya86/redmine_parallel_issue'
   author_url 'http://blog.livedoor.jp/ameya86/'
 end
